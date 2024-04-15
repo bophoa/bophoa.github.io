cc1782caa15e5771cf056962ed28e7aa41220eb8
bophoa <slcbejar@gmail.com>
2024-04-15 11:42:15 -0400

    added javascript page

---
+++ js/homework_javascript.js
@@ -0,0 +1,20 @@
+function mouseOverFUNC()
+	{
+        document.getElementById("myJavascriptButton").src="images/bearblue.png";
+	}
+
+function mouseOutFUNC()
+	{
+	   document.getElementById("myJavascriptButton").src="images/bear.png";
+	}
+
+function mouseDownFUNC()
+	{
+	   document.getElementById("myJavascriptButton").src="assets/down.png";
+	}
+
+function mouseUpFUNC()
+	{
+	   mouseOverFUNC();
+	}
+
