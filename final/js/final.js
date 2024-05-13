f5e1cb87d2148b74b99efdacf858140f3a3deedb
bophoa <slcbejar@gmail.com>
2024-05-13 11:22:21 -0400

    added function mouseover and mouseout

--- final/js/final.js
+++ final/js/final.js
@@ -3,3 +3,33 @@ function alertWithMessage() {
    var message = document.getElementById('alertMessage').value;
    window.alert(message);
 }
+
+function mouseOverFUNCA()
+	{
+        document.getElementById("torchic").src="images/torchic_happy.png";
+	}
+
+function mouseOverFUNCB()
+	{
+        document.getElementById("froakie").src="images/froakie_happy.png";
+	}
+
+function mouseOverFUNCC()
+	{
+        document.getElementById("rowlet").src="images/rowlet_happy.png";
+	}
+
+function mouseOutFUNCA()
+	{
+	   document.getElementById("torchic").src="images/torchic.png";
+	}
+
+function mouseOutFUNCB()
+	{
+	   document.getElementById("froakie").src="images/froakie.png";
+	}
+
+function mouseOutFUNCC()
+	{
+	   document.getElementById("rowlet").src="images/rowlet.png";
+	}
